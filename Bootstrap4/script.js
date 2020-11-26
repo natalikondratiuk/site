bea370de996cafcd0000352b06983fad629c1231
natalikondratiuk <71512255+natalikondratiuk@users.noreply.github.com>
2020-11-26 22:45:53 +0200

    Add files via upload
    Добрий вечір! Додаю нову версію сайту. Намагалася врахувати всі Ваші зауваження, У мене є питання стосовно  відео: немає повноекранного режиму, коли відео перебуває в табличці. Але в окремому блоці повноекранний режим працює нормально. Чи можливо це виправити, щоб і в таблиці був повноекранний режим відео, чи це така властивість сітки Bootstrap? Буду вдячна за відповідь. З повагою, Наталія Кондратюк

--- Bootstrap4/script.js
+++ Bootstrap4/script.js
@@ -1,11 +1,4 @@
-$(.'carousel').carousel({
+('.carousel').carousel({
 	interval: 3000,
 	wrap: false
-})
-
-$(#measure).on('show.bs.modal', function (event) {
-	//let input = $(event.relatedTarget);
-	event.preventDefault();
-	let $this = $(this);
-	let content = $this.data()
-})
+});
